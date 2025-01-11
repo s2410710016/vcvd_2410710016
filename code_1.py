d8edc2f8366e57cb6616dc0545b7eca6576e2716
Gerold Jedinger <s2410710016@fhooe.at>
2025-01-11 08:36:30 +0100

    20250111_0836

--- code_1.py
+++ code_1.py
@@ -2,18 +2,17 @@
 
 import scipy
 from scipy import constants
-import math
 import numpy as np
 import matplotlib.pyplot as plt
 
 #input
 mass_vehicle=2000 #mass of vehicle in kilogram
 alpha_grad=2 #slip angle
-alpha=alpha_grad*math.pi/180 #slip angle in degree
+alpha=alpha_grad*np.pi/180 #slip angle
 my=0.2 #friction coefficient
 
 #definitions
-camber=2 #camber angle
+camber=0 #camber angle
 a1_Fy=-22.1
 a2_Fy=1011.0
 a3_Fy=1078.0
@@ -45,45 +44,59 @@ a13_Fx=0.0
 #General Calculations
 Fz=scipy.constants.g*mass_vehicle/4000 #load on each tire in kilogramm
 
-print ("Fz=",Fz)
+print ("Fz=",round(Fz,2),"kN")
 
 #Side Force
 D_sf=a1_Fy*Fz**2+a2_Fy*Fz #peak factor
-C_sf=1.3
-B_sf=(a3_Fy*math.sin(a4_Fy*math.atan(a5_Fy*Fz))/C_sf*D_sf)*(1-a12_Fy*abs(camber))
-E_sf=a6_Fy*Fz**2+a7_Fy*Fz+a8_Fy
-delta_Sh=a9_Fy*camber
-delta_Sv=(a10_Fy*Fz**2+a11_Fy*Fz)*camber
-phi_sf=(1-E_sf)*(alpha+delta_Sh)+(E_sf/B_sf)*math.atan(B_sf*(alpha+delta_Sh))
-Fy0=D_sf*math.sin(C_sf*math.atan(B_sf*phi_sf))+delta_Sv
+C_sf=1.3 #shape factor
+B_sf=(a3_Fy*np.sin(a4_Fy*np.arctan(a5_Fy*Fz))/(C_sf*D_sf))*(1-a12_Fy*np.abs(camber)) #stiffness factor
+E_sf=a6_Fy*Fz**2+a7_Fy*Fz+a8_Fy #curvature factor
+delta_Sh=a9_Fy*camber #horizontal shift
+delta_Sv=(a10_Fy*Fz**2+a11_Fy*Fz)*camber #vertical shift
 
-print ("Fy0=",Fy0)
+def phi_sf(alpha):
+    return(1-E_sf)*(alpha+delta_Sh)+(E_sf/B_sf)*np.arctan(B_sf*(alpha+delta_Sh))
+
+def Fy(alpha):
+    return D_sf*np.sin(C_sf*np.arctan(B_sf*phi_sf(alpha)))+delta_Sv
+
+print ("Fy=",round(Fy(alpha),2),"N")
 
 #Brake Force
 D_bf=a1_Fx*Fz**2+a2_Fx*Fz #peak factor
-C_bf=1.65
-B_bf=(a3_Fx*Fz**2+a4_Fx*Fz)/(C_bf*D_bf*math.e**(a5_Fx*Fz))
-E_bf=a6_Fx*Fz**2+a7_Fx*Fz+a8_Fx
-phi_bf=(1-E_bf)*alpha+(E_bf/B_bf)*math.atan(B_bf*alpha)
-Fx0=D_bf*math.sin(C_bf*math.atan(B_bf*phi_bf))
-print ("Fx0=",Fx0)
+C_bf=1.65 #shape factor
+B_bf=(a3_Fx*Fz**2+a4_Fx*Fz)/(C_bf*D_bf*np.e**(a5_Fx*Fz)) #stiffness factor
+E_bf=a6_Fx*Fz**2+a7_Fx*Fz+a8_Fx #curvature factor
 
-#Sigma
-kappa=2.3
-Sigma_x=-kappa/(1+kappa)
-Sigma_y=-math.tan(alpha)/(1+kappa)
-Sigma=math.sqrt(Sigma_x**2+Sigma_y**2)
+def phi_bf(alpha):
+    return (1-E_bf)*alpha+(E_bf/B_bf)*np.atan(B_bf*alpha)
+def Fx(alpha):
+    return D_bf*np.sin(C_bf*np.atan(B_bf*phi_bf(alpha)))
+print ("Fx=",round(Fx(alpha),2),"N")
 
-print ("Sigma_X",Sigma_x)
-print ("Sigma_Y",Sigma_y)
+#Sigma definieren
+def Sigma_x(kappa):
+    return -kappa/(1+kappa)
 
-#combined
-Fx=-(Sigma_x/Sigma)*Fx0
-Fy=-(Sigma_y/Sigma)*Fy0
+def Sigma_y(kappa):
+    return -np.tan(alpha)/(1+kappa)
 
-print ("Fx=",Fx)
-print ("Fy=",Fy)
+def Sigma(kappa):
+    return np.sqrt(Sigma_x(kappa)**2+Sigma_y(kappa)**2)
 
-plt.plot(kappa, Fx)
+
+#Funktionen Fx und Fy definieren
+def Fx0(kappa):
+    return -Fx(alpha)*Sigma(kappa)/Sigma_x(kappa)
+
+def Fy0(kappa):
+    return -Fy(alpha)*Sigma(kappa)/Sigma_y(kappa)
+
+#Kappa als Laufvariable
+kappa=np.linspace(0,1,1000)
+
+#Diagramm plotten
+plt.plot(kappa, Fx0(kappa), label='Fx0')
+plt.plot(kappa, Fy0(kappa), label='Fy0')
+plt.legend(loc='best')
 plt.show()
-# %%
